93e3a5e85ea66c5b4c23a4b27a578b6f2014b44c
eusouobn <eusouobn@users.noreply.github.com>
2026-08-03 23:40:47 -0300

    steam: STEAM_FORCE_DESKTOPUI_SCALING=2 (UI 4K com force_zero_scaling)

--- .config/scripts/steam-mangohud.sh
+++ .config/scripts/steam-mangohud.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Script para rodar Steam com MangoHud
+# STEAM_FORCE_DESKTOPUI_SCALING: escala a UI do cliente Steam em 4K
+# (com o force_zero_scaling do XWayland, o cliente abre sem escala)
 
 export MANGOHUD=1
+export STEAM_FORCE_DESKTOPUI_SCALING=2
 exec steam
